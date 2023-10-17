68629f80555860702422d8c107a77f9b30ccfca4
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-10-17 17:26:37 +0100

    source number

---
+++ strncat_obo.c
@@ -0,0 +1,20 @@
+#include "oboshell.h"
+
+/**
+  *strncat_obo - Entry point to the code
+  *@flat: place where the source string is appended
+  *@source: the source
+  *@number: number untill source
+  *Return: pointer to flat
+  */
+char *strncat_obo(char *flat, char *source, int number)
+{
+	int tx;
+	int lgt = strlen_obo(flat);
+
+	for (tx = 0; source[tx] != source[number]; tx++, lgt++)
+	{
+		flat[lgt] = source[tx];
+	}
+	return (flat);
+}
