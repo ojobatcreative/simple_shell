637e95b1bfe36e902ededab9712c173666c503e8
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-10-17 17:24:47 +0100

    copy strong

---
+++ strcpy_obo.c
@@ -0,0 +1,21 @@
+#include "oboshell.h"
+
+/**
+  *strcpy_obo - Entry to the program
+  *@flat: where the source string is to be copied
+  *@source: the source of string
+  *Return: a cpoied string
+  */
+
+char *strcpy_obo(char *flat, char *source)
+{
+	int lgt = 0;
+	int tx;
+
+	while (source[lgt] != '\0')
+		lgt++;
+	for (tx = 0; tx <= lgt; tx++)
+		flat[tx] = source[tx];
+
+	return (flat);
+}
