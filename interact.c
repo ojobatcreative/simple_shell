f526d7a58de23c3b49a650a396f516c564804a7f
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-10-17 16:58:49 +0100

    This is how the shell get's started

---
+++ interact.c
@@ -0,0 +1,45 @@
+#include "oboshell.h"
+
+/**
+ * signal_handle - handles signals
+ * @sig: signal to be handled
+ */
+
+void signal_handle(int sig)
+{
+	if (sig == SIGINT)
+		write(1, "\n(OBOshell) $: ", 14);
+}
+/**
+ * interact_mode - Entry point itno the shell
+ *
+ * Return: always 0 (success)
+  */
+
+void interact_mode(void)
+{
+	char *read = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	int process = 1;
+	char **args;
+
+	signal(SIGINT, signal_handle);
+	while (process == 1)
+	{
+		printf("(OBOshell) $: ");
+		nread = getline(&read, &len, stdin);
+
+		if (nread == -1)
+		{
+			write(1, "Exiting...\n", 12);
+			exit(0);
+		}
+		args = tokenize_line(read);
+
+		process_obo(args);
+
+		free(args);
+	}
+	free(read);
+}
