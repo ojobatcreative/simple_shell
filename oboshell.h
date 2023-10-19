087106bda2edd00e32a882fc2244a80f89eac4c1
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-10-19 09:20:25 +0100

    error corrected

--- oboshell.h
+++ oboshell.h
@@ -28,30 +28,30 @@ typedef struct obo
 } builtinobo;
 
 extern char **environ;
+
 int (*get_obo_func(char *s))(char **);
 int process_obo(char **args);
 int process_mode(char **args, char *read);
 int execute_obo(char **args);
+
 char *handle_path(char *cmd);
+char *strncat_obo(char *flat, char *source, int number);
+char *strdup_obo(char *s);
+char *strcpy_obo(char *flat, char *source);
+char **tokenize_line(char *input, ssize_t read_line);
+char *strcat_obo(char *dest, const char *src);
 
 
 int exit_obo(char **args);
 int env_obo(char **args);
 int cd_obo(char **args);
-char *strcat_obo(char *dest, const char *src);
+int atoi_obo(char *str);
+int execute_mode(char **args, char *read);
 int strcmp_obo(char *strg1, char *strg2);
 int strlen_obo(char *strg);
 
-char *strncat_obo(char *flat, char *source, int number);
-char *strdup_obo(char *s);
-char *strcpy_obo(char *flat, char *source);
-
-
 void process_file(char *file);
 void interact_mode(void);
-char **tokenize_line(char *input, ssize_t read_line);
-int atoi_obo(char *str);
-int execute_mode(char **args, char *read);
 void signal_handle(int sig);
 void handle_multiString(char **args, char *argv);
 
